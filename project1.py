c3209bf9021dbffbfd1bbabce908c2050231223a
sherry6247 <576141501@qq.com>
2018-01-05 00:16:30 +0800

    project 1 modified

--- project1.py
+++ project1.py
@@ -10,44 +10,6 @@ def  getNum(filepath):
             l_list = list(map(int,str))
             num_list.append(l_list)
     return num_list
-# def computeBoxNum(list):
-#     sum_box = 0
-#     sum_box = list[5] + list[4] +list[3] +(list[2]//4)
-#     if list[4] > 0:
-#         if list[0] - list[4]*11 > 0:
-#             list[0] = list[0] - list[4] * 11
-#     if list[3] > 0:
-#         if list[1] - list[3] * 5 > 0:
-#             list[1] = list[1] - list[3] * 5
-#         if list[0] - list[3]
-#     if list[2] > 0:
-#         if (list[2]%4) == 3:
-#             if list[0] >= 5:
-#                 list[0] = list[0] - 5
-#             if list[1] >= 1:
-#                 list[1] = list[1] -1
-#             sum_box += 1
-#         if (list[2]%4) == 2:
-#             if list[1] >= 3:
-#                 list[1] = list[1] - 3
-#             if list[0] >= 6:
-#                 list[0] = list[0] - 6
-#             sum_box += 1
-#         if (list[2]%4) == 1:
-#             if list[1] >= 5:
-#                 list[1] = list[1] - 5
-#             if list[0] >= 7:
-#                 list[0] = list[0] - 7
-#             sum_box += 1
-#     if (list[1]%9) > 0:
-#         sum_box = sum_box + list[1]//9 + 1
-#     else:
-#         sum_box = sum_box + list[1]//9
-#     if (list[0]%36) > 0:
-#         sum_box = sum_box + list[0]//36 + 1
-#     else:
-#         sum_box = sum_box + list[0]//36
-#     return sum_box
 def computeBoxNum(list):
     sum_box = 0
     threeBox = [0,5,3,1]
@@ -66,4 +28,3 @@ if __name__ == '__main__':
     for n_list in num_list:
         sum = computeBoxNum(n_list)
         print(sum)
-
